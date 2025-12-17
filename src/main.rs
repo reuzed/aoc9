c229a9e3018fd86cadf56678b5e6c4526d9eecee
reuzed <49994279+reuzed@users.noreply.github.com>
2025-12-17 15:23:17 +0000

    part 2 failures

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use itertools::{Itertools, iproduct};
 
+use raqote::*;
 #[derive(Clone)]
 #[derive(Debug)]
 struct Point {
@@ -53,15 +54,23 @@ fn green_tiles(p1: &Point, p2: &Point, points: &Vec<Point>) -> bool {
     // We can guarantee that no point is inside the rectangle
     // Ignore annoying case where 2 segments are adjacent
     for (i1, tp1) in points.iter().enumerate(){
-        let i2 = (i1+1)% points.len();
+        let i2 = (i1+1) % points.len();
         let tp2 = &points[i2];
-        // 
+        // test points are between in the x direction so a vertical segment
         if is_ordered(p1.x, tp1.x, p2.x) && is_ordered(p1.x, tp2.x, p2.x) 
-            && is_weak_ordered_4(tp1.y, p1.y, p2.y, tp2.y){
+            && tp1.x == tp2.x
+            && (is_weak_ordered_4(tp1.y, p1.y, p2.y, tp2.y)
+              || is_weak_ordered_4(tp2.y, p1.y, p2.y, tp1.y)
+            ){
                 return false
         }
+        // test points are between in the y direction so a horizontal segment
         if is_ordered(p1.y, tp1.y, p2.y) && is_ordered(p1.y, tp2.y, p2.y) 
-            && is_weak_ordered_4(tp1.x, p1.x, p2.x, tp2.x){
+            && tp1.y == tp2.y
+            && (
+                is_weak_ordered_4(tp1.x, p1.x, p2.x, tp2.x)
+                || is_weak_ordered_4(tp2.x, p1.x, p2.x, tp1.x)
+            ){
                 return false
         }
     }
@@ -111,7 +120,8 @@ fn main() {
     let good_rects = iproduct!(points.clone(), points.clone())
       .filter(|(p1, p2)| !point_inside(p1, p2, &points))
       .filter(|(p1,p2)| green_tiles(p1,p2, &points))
-      .filter(|(p1,p2)| is_inner_rect(p1,p2, &points));
+    //   .filter(|(p1,p2)| is_inner_rect(p1,p2, &points))
+      ;
     let green_max= good_rects.clone()
       .map(|(p1, p2)| p1.area(&p2))
       .max()
@@ -119,4 +129,125 @@ fn main() {
     println!("Green max {:?}", green_max);
     let best_points = good_rects.filter(|(p1, p2)| p1.area(&p2) == green_max);
     println!("Best points: {:?}", best_points.collect::<Vec<(Point, Point)>>())
+}
+
+fn _main(){
+    // For visualisation
+    let filename = "input.txt".to_string();
+    // let filename = "example.txt".to_string();
+
+    let RES_X: i32 = 1000;
+    let RES_Y: i32 = 1000;
+    let points = read_input(filename.clone()); 
+    let max_point = Point{
+        x: points.iter().map(|p| p.x).max().unwrap(),
+        y: points.iter().map(|p| p.y).max().unwrap(),
+    };
+    // let min_point = Point{
+    //     x: points.iter().map(|p| p.x).min().unwrap(),
+    //     y: points.iter().map(|p| p.y).min().unwrap(),
+    // };
+
+    let x_scale = (1.1*(max_point.x - 0) as f64)/(RES_X as f64);
+    let y_scale = (1.1*(max_point.y - 0) as f64)/(RES_Y as f64);
+
+    let mut dt = DrawTarget::new(RES_X, RES_Y);
+
+    let mut pb = PathBuilder::new();
+    pb.move_to(((points[0].x as f64)/x_scale) as f32, ((points[0].y as f64)/y_scale) as f32);
+    for point in points.iter(){
+        pb.line_to(((point.x as f64)/x_scale) as f32, ((point.y as f64)/y_scale) as f32);
+    }
+    pb.close();
+    let path = pb.finish();
+
+    let source= Source::Solid(SolidSource {
+        r: 0x00,
+        g: 0xFF,
+        b: 0x00,
+        a: 0xFF,
+    });
+    dt.fill(&path, &source, &DrawOptions::new());
+
+    // draw circles at points
+    let source= Source::Solid(SolidSource {
+        r: 0xFF,
+        g: 0x00,
+        b: 0x00,
+        a: 0xFF,
+    });
+
+    for point in points.iter(){
+        let radius = 3.0;
+        let mut pb = PathBuilder::new();
+        pb.arc(((point.x as f64)/x_scale) as f32, ((point.y as f64)/y_scale) as f32, radius, 0., 2. * std::f32::consts::PI);
+        pb.close();
+        let path = pb.finish();
+        dt.fill(&path, &source, &DrawOptions::new());
+    }
+
+
+    let new_filename = format!(
+        "{}_blank_img.png",
+        filename.clone().strip_suffix(".txt").unwrap_or(&filename)
+    );
+    let _ = dt.write_png(new_filename);
+
+    // Draw in winning rect:
+
+    // let point_1 = Point { x: 83903, y: 85123 };
+    // let point_2 = Point { x: 15099, y: 16084 };
+
+    // let point_1 = Point { x: 16155, y: 83805 };
+    // let point_2 = Point { x: 84138, y: 15900 };
+
+    // let point_1 = Point { x: 94870, y: 48753 };
+    // let point_2 = Point { x: 5606, y: 67703 };
+
+    let point_1 = Point { x: 5606, y: 67703 };
+    let point_2 = Point { x: 94870, y: 50025 };
+
+    // let point_1 = Point { x: 9, y: 5 };
+    // let point_2 = Point { x: 2, y: 3 };
+
+    let rect_points: Vec<Point> = vec![
+        Point{
+            x: std::cmp::max(point_1.x, point_2.x),
+            y: std::cmp::max(point_1.y, point_2.y),
+        },
+        Point{
+            x: std::cmp::max(point_1.x, point_2.x),
+            y: std::cmp::min(point_1.y, point_2.y),
+        },
+        Point{
+            x: std::cmp::min(point_1.x, point_2.x),
+            y: std::cmp::min(point_1.y, point_2.y),
+        },
+        Point{
+            x: std::cmp::min(point_1.x, point_2.x),
+            y: std::cmp::max(point_1.y, point_2.y),
+        },
+    ];
+
+    let mut pb = PathBuilder::new();
+    pb.move_to(((rect_points[0].x as f64)/x_scale) as f32, ((rect_points[0].y as f64)/y_scale) as f32);
+    for point in rect_points.iter(){
+        pb.line_to(((point.x as f64)/x_scale) as f32, ((point.y as f64)/y_scale) as f32);
+    }
+    pb.close();
+    let path = pb.finish();
+
+    let source= Source::Solid(SolidSource {
+        r: 0x00,
+        g: 0x00,
+        b: 0xFF,
+        a: 0xFF,
+    });
+    dt.fill(&path, &source, &DrawOptions::new());
+
+    let new_filename = format!(
+        "{}_img.png",
+        filename.clone().strip_suffix(".txt").unwrap_or(&filename)
+    );
+    let _ = dt.write_png(new_filename);
 }
